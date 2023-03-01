77f225d43b01e4ef217a415e9223717156607eb6
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-03-01 14:03:22 +0100

    ex 1

---
+++ hw.js
@@ -0,0 +1,15 @@
+
+
+//Exercise 1
+function sum(int1, int2){
+    if(int1 === int2){
+        return (int1 + int2) * 3
+    }
+    else {
+        return int1 + int2
+    }
+}
+console.log(sum(5, 8));
+
+//Exercise 2
+
