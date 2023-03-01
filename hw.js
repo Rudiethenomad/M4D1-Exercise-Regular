4b76175d6a53ed4b3fb9335de344eb3212bdb12f
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-03-01 14:24:36 +0100

    ex 3

--- hw.js
+++ hw.js
@@ -17,3 +17,11 @@ function numCheck(n1, n2) {
     return n1 === 50 || n2 === 50 || n1 + n2 === 50
   }
   console.log(numCheck(50, 1));
+
+  //Exercise 3
+  function remove(str, plc){
+    return str.slice(0, plc -1) + str.slice(plc)
+  }
+    console.log(remove("I want a beer", 8))
+
+//Exercise 4
