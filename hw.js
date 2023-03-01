86b695e401739eed7e0c4ddddba63542d592ff2a
Rudiethenomad <111630606+Rudiethenomad@users.noreply.github.com>
2023-03-01 15:21:06 +0100

    Update hw.js

--- hw.js
+++ hw.js
@@ -25,3 +25,17 @@ function numCheck(n1, n2) {
     console.log(remove("I want a beer", 8))
 
 //Exercise 4
+function largestInt(in1, in2, in3)
+{
+    if (in1 > in2 && in1 > in3){
+        return in1
+    }
+    else if (in2 > in1 && in2 > in3){
+        return in2
+    }
+    else if (in3 > in1 && in3 > in2){
+        return in3
+    }
+
+}
+console.log(largestInt(8, 10, 1));
